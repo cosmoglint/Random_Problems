556851964f5f399443862033b31b0b1da849ed9c
cosmoglint <ramprakashsmail@gmail.com>
2021-03-26 14:33:45 +0530

    ADD:  testing

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,2 @@
 
-
-# data structure - dictionary
-# data structure that has the required resource, with a timer.
-#  when timer expires, clear the data from cache
-
-
-# we start with first number
+#  create a cache with dictionary.
