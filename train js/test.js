637a3c50b43dc5e2662d88d4505732bc6e6fb2d4
ramprakash <ramprakashsmail@gmail.com>
2021-04-09 20:53:35 +0530

    ADD:  train js and testing

---
+++ train js/test.js
@@ -0,0 +1,28 @@
+let chuckapi= 'https://api.chucknorris.io/jokes/random'
+promises=[]
+
+
+// asadas
+for (let i=0;i<50;i++){
+  getapi()
+  .then((ans)=>{
+    fin = ans.replace('Chuck Norris','Suriyaaaaa')
+    document.getElementById("main").innerHTML += "<br />" + fin;
+
+  })
+}
+// Promise.all(promises)
+// .then((ans)=>{
+//   for (let i=0;i<promises.length;i++){
+//     console.log(ans[i]);
+//     document.getElementById("main").innerHTML += "<br />" + ans[i];
+
+
+  // }
+// })
+async function getapi(){
+  let res= await fetch(chuckapi)
+  let data= await res.json()
+  let value= await data.value
+  return value
+}
