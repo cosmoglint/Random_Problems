cd1728ef3e6de3e5d0179446abe3b269c41daa62
ramprakash <ramprakashsmail@gmail.com>
2021-05-11 22:58:54 +0530

    ADD:  deep copy and mapper

---
+++ python_coder/mapper.py
@@ -0,0 +1,4 @@
+themap = map(int,"hello buddy this is a string tot list".split())
+
+
+print(list(themap))
--- python_coder/perms.py
+++ python_coder/perms.py
@@ -1,16 +1,26 @@
 
-lst = ["act","god","cat","dog","tac"]
+lst = ["act","god","cat","dog","tac","tac"]
 
 
 mydict = {}
+#
+#
+# for i in lst:
+#     newl = [ x for x in i]
+#     thes = frozenset(newl)
+#
+#     # thes = set(newl)
+#     if thes not in mydict:
+#         mydict[thes] = 1
+#     else:
+#         mydict[thes] += 1
+#
+# print(mydict)
 
 
-for i in lst:
-    newl = [ x for x in i]
-    thes = frozenset(newl)
-    if thes not in mydict:
-        mydict[thes] = 1
-    else:
-        mydict[thes] += 1
 
-print(mydict)
+
+
+
+thes = frozenset([1,23,3])
+print(type(thes))
