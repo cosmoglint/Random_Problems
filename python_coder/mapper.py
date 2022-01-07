92cbd0ae980a903cb3281411f80614bfe6b70dcc
cosmoglint <ramprakashsmail@gmail.com>
2022-01-07 23:41:06 +0530

    ADD:  random stuff

--- python_coder/mapper.py
+++ python_coder/mapper.py
@@ -1,4 +1,12 @@
-themap = map(int,"hello buddy this is a string tot list".split())
+# themap = map(int,"hello buddy this is a string tot list".split())
 
 
-print(list(themap))
+#print(list(themap))
+lst = [i for i in range(10)]
+
+val = 0
+for i in lst:
+    breakpoint()
+    val += i
+    
+print(lst)
